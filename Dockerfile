0f76b9d7bb60747187d6b6c8946a43d057c74c9a
Christos <csidirop@runbox.com>
2022-06-28 14:35:31 +0200

    Working docker compose update

--- Dockerfile
+++ Dockerfile
@@ -1,40 +1,23 @@
-# Use local typo3 v9 all-in-one base image based on Apache2 on Debian 11 bullseye
-# https://github.com/csidirop/typo3-docker/tree/typo3-v9.x-allinone
-FROM csidirop/typo3-v9:9.5
+# Use local typo3 v9 base image based on Apache2 on Debian 11 bullseye
+# https://github.com/csidirop/typo3-docker/tree/typo3-v9.x
+FROM csidirop/typo3-v9:9.5-compose
 
 LABEL authors='Christos Sidiropoulos <Christos.Sidiropoulos@uni-mannheim.de>'
+
+ENV DB_ADDR=localhost
+ENV DB_PORT=3306
 
 EXPOSE 80
 
 # This Dockerfile aimes to install a working typo3 v9 instance with the kitodo/presentation extension
 # based on this guide: https://github.com/UB-Mannheim/kitodo-presentation/wiki
 
-# Setup Typo3 with typo3console (https://docs.typo3.org/p/helhum/typo3-console/main/en-us/CommandReference/InstallSetup.html):
-WORKDIR /var/www/typo3/
-RUN docker-php-ext-install -j$(nproc) mysqli
-RUN service mariadb start \
-  && vendor/bin/typo3cms install:setup \ 
-    --use-existing-database \
-    --database-driver='mysqli' \
-    --database-user-name='typo3' \
-    --database-user-password='password' \
-    --database-host-name='127.0.0.1' \
-    --database-port=3306 \
-    --database-name='typo3' \
-    --admin-user-name='test' \
-    --admin-password='test1234' \
-    --site-setup-type=no \
-    --site-name presentation \
-    --web-server-config=apache
-
-# Install Kitodo.Presentation:
-RUN service mariadb start \ 
- && composer config platform.php 7.4 \
- && composer require kitodo/presentation:^3.3 \
- && vendor/bin/typo3 extensionmanager:extension:install dlf \
- && chown -R www-data:www-data .
-
 # Define datavolumes:
 #VOLUME /var/www/html/fileadmin
 #VOLUME /var/www/html/typo3conf
 #VOLUME /var/www/html/typo3temp
+
+# Copy startup script into the container:
+COPY docker-entrypoint.sh /
+# Run startup script & start apache2 (https://github.com/docker-library/php/blob/master/7.4/bullseye/apache/apache2-foreground)
+CMD /docker-entrypoint.sh & apache2-foreground 
---
+++ docker-entrypoint.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# Work in progress!
+
+echo '[MAIN] Running startup script:'
+
+# Get waiting script
+echo '[MAIN] get waiting script:'
+apt-get update
+apt-get install -y wget
+wget -q https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh 
+# Cleanup:
+echo '[MAIN] cleanup:'
+apt-get purge -y wget
+apt-get autoremove -y 
+apt-get clean 
+rm -rf /var/lib/apt/lists/*
+# Wait for database container:
+chmod +x wait-for-it.sh
+./wait-for-it.sh -t 0 ${DB_ADDR}:${DB_PORT}
+
+# Setup Typo3 with typo3console (https://docs.typo3.org/p/helhum/typo3-console/main/en-us/CommandReference/InstallSetup.html):
+cd /var/www/typo3/
+docker-php-ext-install -j$(nproc) mysqli
+echo '[MAIN] Auto setup typo3:'
+vendor/bin/typo3cms install:setup --use-existing-database --database-driver='mysqli' --database-user-name='typo3' --database-user-password='password' --database-host-name='db' --database-port=3306 --database-name='typo3-presentation-v3' --admin-user-name='test' --admin-password='test1234' --site-setup-type=no --site-name presentation --web-server-config=apache
+# TODO: linebreaks not working, but making it more readeble:
+# vendor/bin/typo3cms install:setup \ 
+#     --use-existing-database \
+#     --database-driver='mysqli' \
+#     --database-user-name='typo3' \
+#     --database-user-password='password' \
+#     --database-host-name='db' \
+#     --database-port=3306 \
+#     --database-name='typo3-presentation-v3' \
+#     --admin-user-name='test' \
+#     --admin-password='test1234' \
+#     --site-setup-type=no \
+#     --site-name presentation \
+#     --web-server-config=apache
+
+# Install Kitodo.Presentation v3.3:
+echo '[MAIN] Install presentation 3.3:'
+composer config platform.php 7.4 
+composer require kitodo/presentation:^3.3 
+vendor/bin/typo3 extensionmanager:extension:install dlf 
+chown -R www-data:www-data .    
+
+# Check status:
+echo '[MAIN] Check apache status:'
+service apache2 status
+
+echo '[MAIN] Finished setup: http://localhost/typo3/ '
