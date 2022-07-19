387beda8ad0b0498bab429dcb9b913ca4b2b20d7
Christos Sidiropoulos <csidirop@runbox.com>
2022-07-19 16:15:22 +0200

    Create site configuration
    Signed-off-by: Christos Sidiropoulos <csidirop@runbox.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -88,6 +88,9 @@ if [ ! -f /initFinished ]; then
     # .... TODO ....
     ## Embed links: 2 main site header or footer
     # .... TODO ....
+    # Create Site configuration with two languages (en & de):
+    mkdir -p config/sites/dfgviewer/
+    echo -e "base: '/'\nbaseVariants: {  }\nerrorHandling: {  }\nlanguages:\n  -\n    title: 'DFG-Viewer (german)'\n    enabled: true\n    base: '/'\n    typo3Language: de\n    locale: de_DE.UTF-8\n    iso-639-1: de\n    navigationTitle: DFG-Viewer\n    hreflang: de-DE\n    direction: ''\n    flag: de\n    languageId: '0'\n  -\n    title: 'DFG-Viewer (Englisch)'\n    enabled: true\n    base: /en/\n    typo3Language: default\n    locale: en_US.UTF-8\n    iso-639-1: en\n    navigationTitle: 'DFG-Viewer (English)'\n    hreflang: en-US\n    direction: ''\n    fallbackType: strict\n    fallbacks: '0'\n    flag: gb\n    languageId: '1'\nrootPageId: 1\nroutes: {  }\n" >> config/sites/dfgviewer/config.yaml
 
     # AdditionalConfiguration (Fixes TYPO3-CORE-SA-2020-006: Same-Origin Request Forgery to Backend User Interface: https://typo3.org/security/advisory/typo3-core-sa-2020-006)
     # (Only if DMZ is set in .env)
