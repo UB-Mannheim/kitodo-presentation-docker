be1d0b6680a840761c893a41df30a343e38707f6
Christos Sidiropoulos <csidirop@runbox.com>
2022-09-20 15:43:30 +0200

    Fix wrong path after merge
    Signed-off-by: Christos Sidiropoulos <csidirop@runbox.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -111,7 +111,7 @@ if [ ! -f /initFinished ]; then
     envsubst '${HOST}' < /data/config.yaml >> /var/www/typo3/config/sites/presentation/config.yaml
     if [ ${HOST} = 'localhost' ]; then
         ### Replace localhost with / :
-        sed -i 's/localhost/\//g' /var/www/typo3/config/sites/dfgviewer/config.yaml
+        sed -i 's/localhost/\//g' /var/www/typo3/config/sites/presentation/config.yaml
     fi
     chown -R www-data:www-data config
     ## Insert translated pages and content elements as translations:
