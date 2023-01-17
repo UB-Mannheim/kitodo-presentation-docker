8c71f5a71b8e85394d1c88d1b6f2d6be31d7a0f7
Christos Sidiropoulos <csidirop@runbox.com>
2023-01-17 17:39:16 +0100

    Use mounted volumes instead of on demand download  - pre download the needed resources and mount them into the container
    Signed-off-by: Christos Sidiropoulos <csidirop@runbox.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -79,6 +79,8 @@ if [ ! -f /initFinished ]; then
     find public/ -name index.html -exec chmod -v 0660 {} \;             # set permissions for index.html: owner and group can read and write
     ## Presentation options:
     vendor/bin/typo3cms configuration:set EXTENSIONS/dlf/fileGrpImages 'DEFAULT,DEFAULTPLUS,MASTER,MAX,ORIGINAL' # Add additional fileGrps: ORIGINAL (SLUB), MASTER (TU Braunschweig), DEFAULTPLUS (UB HD)
+    ## Solr options:
+    vendor/bin/typo3cms configuration:set EXTENSIONS/dlf/solrHost "solr" # Inside the container solr is reacheble under 'solr'
     ## OCR-On-Demand options:
     vendor/bin/typo3cms configuration:set EXTENSIONS/dlf/fulltextFolder 'fileadmin/fulltextFolder'
     vendor/bin/typo3cms configuration:set EXTENSIONS/dlf/fulltextTempFolder 'fileadmin/_temp_/ocrTempFolder/fulltext'
@@ -169,5 +171,6 @@ if [ ! -f /initFinished ]; then
     echo -e "${CLR_B}[MAIN]${CLR_G} Finished setup!${NC}"
 fi
 
-echo -e "${CLR_B}[MAIN]${CLR_G} Site http://${HOST} ${NC}"
+echo -e "${CLR_B}[MAIN]${CLR_G} Site:    http://${HOST} ${NC}"
 echo -e "${CLR_B}[MAIN]${CLR_G} Backend: http://${HOST}/typo3/ ${NC}"
+echo -e "${CLR_B}[MAIN]${CLR_G} Solr:    http://${HOST}:8983 ${NC}"
